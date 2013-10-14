2ebc97ed13bf6ac8d8bdd8001d142ccfcdba4d74
dan <dan@dan-Aspire-5251.(none)>
2013-10-13 21:16:15 -0700

    Integrated Custom Shaders

--- externals/gl_libs.h
+++ externals/gl_libs.h
@@ -2,9 +2,12 @@
 
 #include "SOIL.h"
 
+#include <GL/glew.h>
+
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <GL/glext.h>
 
 #include <string.h>
 #include <sstream>
--- shaders/main.frag
+++ shaders/main.frag
@@ -1,4 +1,5 @@
 
 void main()
 {
+ gl_FragColor = gl_Color; //vec4(0.0,1.0,1.0,1.0);
 }
